715cb411bb59050dfd15811aae2127e203f0c8a9
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-26 13:11:47 -0500

    commit

--- src/main/java/pe/com/nttdata/SpringBootConfigServerApplication.java
+++ src/main/java/pe/com/nttdata/SpringBootConfigServerApplication.java
@@ -1,9 +1,26 @@
+/**
+ * Resumen.
+ * Objeto                   : SpringBootConfigServerApplication.java
+ * Descripción              : Clase para iniciar el configserver.
+ * Fecha de Creación        : 08/08/2022.
+ * Proyecto de Creación     : Bootcamp-30.
+ * Autor                    : Oscar Candela.
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Modificaciones
+ * Motivo                   Fecha             Nombre                  Descripción
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Bootcamp-30              17/08/2022        Mario Vásquez           Realizar la creación de un método nuevo.
+ */
+
 package pe.com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Clase para iniciar el configserver.
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class SpringBootConfigServerApplication {
